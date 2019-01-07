ae42580055f8310b6d6cbec7bbafae4f6b3ae01f
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-01-06 19:42:43 -0500

    Just some simple update

--- scripts/netbricks-setup.sh
+++ scripts/netbricks-setup.sh
@@ -21,8 +21,8 @@ sudo apt-get install -y musl-tools
 # setup rust
 curl https://sh.rustup.rs -sSf | sh  # Install rustup
 source $HOME/.cargo/env
-rustup install nightly-2019-01-03
-rustup default nightly
+rustup install nightly-2019-01-02
+rustup default nightly-2019-01-02
 
 # git repo
 mkdir git && cd git && git clone https://github.com/jethrosun/NetBricks.git -b fix-dpdk netbricks
