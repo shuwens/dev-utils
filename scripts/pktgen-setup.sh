feac730974730da9c1bc041fbd5b832d4ccf3e42
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-02-23 16:41:59 -0500

    Update: pktgen

--- scripts/pktgen-setup.sh
+++ scripts/pktgen-setup.sh
@@ -8,11 +8,10 @@ sudo apt-get update
 # to make sure we have everything
 sudo apt-get install -y git curl
 
-# make my life easy
-sudo apt-get install -y ranger bash-completion vim build-essential neovim silversearcher-ag
 
 # install ubuntu deps
-sudo apt-get install -y libgnutls30 libgnutls-openssl-dev libcurl4-gnutls-dev libnuma-dev libpcap-dev  libsctp-dev gcc
+#sudo apt-get install -y libgnutls30 libgnutls-openssl-dev libcurl4-gnutls-dev libnuma-dev  libsctp-dev gcc libprotobuf-c-dev
+sudo apt-get install -y libpcap-dev libprotobuf-c-dev
 
 # install kernel headers
 sudo apt-get install -y linux-headers-$(uname -r)
@@ -20,8 +19,8 @@ sudo apt-get install -y linux-headers-$(uname -r)
 # install clang 7.0 and cmake
 sudo apt-get install -y clang-5.0 cmake
 
-# musl
-sudo apt-get install -y musl-tools
+# for helping pktgen
+sudo apt-get install -y autoconf build-essential
 
 # git repo
-mkdir -p ~/dev && cd ~/dev && git clone https://github.com/melvinw/pktgen.git
+mkdir -p ~/dev && cd ~/dev && git clone https://github.com/jethrosun/pktgen.git
