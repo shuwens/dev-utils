f0a777c4aeee23456d1af72da953210ff536be38
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-03-12 13:41:21 -0400

    Revert "just testing, remember to revert"

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -5,6 +5,11 @@ sudo apt-get install -y python-virtualenv python3-dev python3 python3-venv
 
 mkdir -p ~/.config && cd ~/.config && git clone  https://github.com/jethrosun/yavc.git -b neovim nvim
 
+curl https://sh.rustup.rs -sSf | sh  # Install rustup
+
+source $HOME/.cargo/env
+
+
 rustup install nightly
 rustup default nightly
 rustup component add rls rust-analysis rust-src
