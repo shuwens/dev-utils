5c4c5a067784b03de46e0c02d47bc1108ce34b8c
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-03-16 16:42:00 -0400

    pktgen-dpdk setup * scripts to bootstrap the env * various cfg

--- pktgen-dpdk/setup.sh
+++ pktgen-dpdk/setup.sh
@@ -3,4 +3,10 @@
 set -e
 
 ln -s $PWD/run.sh           ~/dev/pktgen-dpdk/run.sh
-ln -s $PWD/cfg/default.cfg  ~/dev/pktgen-dpdk/cfg/default.cfg
+echo "setup run.sh for pktgen-dpdk/..."
+
+#rm ~/dev/pktgen-dpdk/cfg/default.cfg
+#ln -s $PWD/cfg/default.cfg  ~/dev/pktgen-dpdk/cfg/default.cfg
+rm ~/dev/pktgen-dpdk/cfg/*
+cp -r $PWD/cfg/  ~/dev/pktgen-dpdk/cfg/
+echo "use the default cfg in our case..."
