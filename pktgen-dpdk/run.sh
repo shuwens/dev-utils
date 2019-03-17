8c7c2718586a1cdb7331088437f6cb45393079bd
jethros <jethros@saul.ccs.neu.edu>
2019-03-17 15:50:27 -0400

    run.sh

--- pktgen-dpdk/run.sh
+++ pktgen-dpdk/run.sh
@@ -2,5 +2,9 @@
 
 # try 1
 #sudo -E app/x86_64-native-linuxapp-gcc/pktgen -c 0x1f -n 3 -- -P -m "[1:3].0, [2:4].1"
+
 # try again
-sudo -E app/x86_64-native-linuxapp-gcc/pktgen -l 0-4 -n 3 -- -P -m "[1:3].0, [2:4].1"
+#sudo -E app/x86_64-native-linuxapp-gcc/pktgen -l 0-4 -n 3 -- -P -m "[1:3].0, [2:4].1"
+
+# run py
+./tools/run.py  default
