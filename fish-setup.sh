9ddb11dd8c3562ee6587638e3895f953dabd9626
jethrosun <jethro.s.sun@gmail.com>
2020-01-08 16:21:44 -0500

    update

--- fish-setup.sh
+++ fish-setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+sudo apt install fish -y 
 
 mkdir -p ~/.config/fish
 ln -s $PWD/config.fish ~/.config/fish/config.fish
 echo "fish setup finished"
+fish
 
 rm ~/.screenrc
 ln -s $PWD/.screenrc  ~/.screenrc
--- setup-scripts/dev-setup.sh
+++ setup-scripts/dev-setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -ex
 
-sudo apt-get install -y python-virtualenv python3-dev python3 python3-venv
+sudo apt-get install -y python-virtualenv python3-dev python3 python3-venv python3-pip
 
 curl -sL install-node.now.sh/lts | sudo bash
 # Optional install yarn if you want install extension by CocInstall command
 curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
 
-pip install neovim
+pip3 install neovim
 #pip3 install neovim
 python3 -m pip install neovim
 
 mkdir -p ~/.config && cd ~/.config && git clone  https://github.com/jethrosun/yavc.git -b neovim nvim
-
