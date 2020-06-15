1edbb53c5aadbf9b14a7734a8e236b5361add1b9
jethrosun <jethro.rules@real.com>
2020-06-15 00:30:21 -0400

    Changed files w/ little update

--- fish-setup.sh
+++ fish-setup.sh
@@ -4,9 +4,11 @@ sudo apt install fish -y
 mkdir -p ~/.config/fish
 ln -s $PWD/config.fish ~/.config/fish/config.fish
 echo "fish setup finished"
-fish
 
 rm ~/.screenrc
 ln -s $PWD/.screenrc  ~/.screenrc
 echo "screen setup finished"
 
+chsh -s `which fish`
+
+fish
