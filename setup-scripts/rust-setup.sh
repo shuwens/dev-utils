c6e5f507751ae59363100a5a5db8ffbde2180ab2
Shuwen Jethro Sun <jethro.sun7@gmail.com>
2020-08-29 15:23:18 -0400

    ajax-loader hotness, oh yeah

--- setup-scripts/rust-setup.sh
+++ setup-scripts/rust-setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install libssl-dev pkg-config cmake zlib1g-dev
+sudo apt-get install libssl-dev pkg-config cmake zlib1g-dev -y
 
 
 if [ -e $HOME/.cargo/ ]; then
@@ -19,12 +19,12 @@ fi
 
 
 rustup component add rust-src rustfmt
-rustup component add clippy rls rust-analysis
-
-cargo install --force cargo-tree
-cargo install --force cargo-tarpaulin
-cargo install --force cargo-udeps
-cargo install --force cargo-deny
-cargo install --force cargo-sweep
-cargo install --force cargo-readme
-cargo install --force --git https://github.com/kbknapp/cargo-outdated
+# rustup component add clippy rls rust-analysis
+#
+# cargo install --force cargo-tree
+# cargo install --force cargo-tarpaulin
+# cargo install --force cargo-udeps
+# cargo install --force cargo-deny
+# cargo install --force cargo-sweep
+# cargo install --force cargo-readme
+# cargo install --force --git https://github.com/kbknapp/cargo-outdated
