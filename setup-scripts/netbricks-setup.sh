08dd12aa815f16bd85f98a29ff7b8c5a45746b5d
Shuwen Jethro Sun <jethro.rules@real.com>
2020-11-01 23:54:37 -0500

    Don’t mess with Voodoo

--- setup-scripts/netbricks-setup.sh
+++ setup-scripts/netbricks-setup.sh
@@ -36,6 +36,17 @@ sudo apt-get install -y musl-tools numactl gdb
 sudo apt install -y libssl-dev libevent-dev
 sudo apt install -y libclang-dev
 
+sudo apt install -y jq
+
+# bcc
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
+echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/iovisor.list
+
+sudo apt-get update
+sudo apt-get install bpfcc-tools linux-headers-$(uname -r)
+sudo apt-get install bcc-tools libbcc-examples linux-headers-$(uname -r)
+
+
 if [ -e $HOME/dev/netbricks ]; then
 	echo "Passing, netbricks exists.."
 else
