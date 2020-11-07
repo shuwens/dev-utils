cd69ce4db7de4bc3298c796dda3f546b96006c10
Shuwen Jethro Sun <jethro.rules@real.com>
2020-11-07 11:20:44 -0500

    put code that worked where the code that didn't used to be

--- setup-scripts/dev-setup.sh
+++ setup-scripts/dev-setup.sh
@@ -15,7 +15,7 @@ pip3 install neovim
 #pip3 install neovim
 python3 -m pip install neovim
 
-cd ~/dev/others/rust-analyzer || (mkdir -p ~/dev/others && cd ~/dev/others && git clone https://github.com/rust-analyzer/rust-analyzer.git)
+cd ~/dev/others/rust-analyzer || (mkdir -p ~/dev/others && cd ~/dev/others && git clone https://github.com/rust-analyzer/rust-analyzer.git && cd ~/dev/others/rust-analyzer)
 cargo xtask install --server
 
 cd ~/.config/nvim || (mkdir -p ~/.config && cd ~/.config && git clone  https://github.com/jethrosun/yavc.git  nvim)
