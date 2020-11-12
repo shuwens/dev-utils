c9d4631d1616e712e9cb98a70714c7c66a6479f0
Shuwen Jethro Sun <jethro.rules@real.com>
2020-11-12 11:02:02 -0500

    All your codebase are belong to us.

--- setup-scripts/netbricks-setup.sh
+++ setup-scripts/netbricks-setup.sh
@@ -15,16 +15,19 @@ sudo apt-get install -y ranger bash-completion vim build-essential neovim silver
 
 # Everything we need to make NetBricks work
 
-sudo apt-get install -y git curl libssl-dev pkg-config #dpdk-igb-uio-dkms
+# sudo apt-get install -y git curl libssl-dev pkg-config #dpdk-igb-uio-dkms
 # install ubuntu deps
-sudo apt-get install -y libgnutls30 libgnutls-openssl-dev libcurl4-gnutls-dev libnuma-dev libpcap-dev  libsctp-dev gcc
+# sudo apt-get install -y libgnutls30 libgnutls-openssl-dev libcurl4-gnutls-dev libnuma-dev libpcap-dev  libsctp-dev gcc
 # install kernel headers
-sudo apt-get install -y linux-headers-"$(uname -r)"
+# sudo apt-get install -y linux-headers-"$(uname -r)"
 # install clang with correct version and cmake
-sudo apt-get install -y apt-utils build-essential pciutils linux-headers-"$(uname -r)" python python3 libssl-dev
-sudo apt-get install -y libgnutls30 libcurl4-gnutls-dev cmake bash libpcap-dev libnuma-dev llvm libelf-dev
-sudo apt-get install -y libssl-dev pkg-config libgnutls-openssl-dev libcurl4-gnutls-dev libsctp-dev gcc
-sudo apt-get install -y clang-6.0 cmake musl-tools libssl-dev libevent-dev
+
+sudo apt-get install -y apt-utils build-essential pciutils linux-headers-"$(uname -r)" python python3
+sudo apt-get install -y cmake musl-tools cmake bash llvm gcc pkg-config libclang-dev
+sudo apt-get install -y libgnutls30 libcurl4-gnutls-dev libpcap-dev libnuma-dev libelf-dev
+sudo apt-get install -y libssl-dev libgnutls-openssl-dev libcurl4-gnutls-dev libsctp-dev libevent-dev
+# FIXME: careful with clang and LLVM, currently only LLVM 3.8 works
+# sudo apt-get install -y clang-6.0
 
 # rdr browser
 sudo apt-get install -y chromium-chromedriver
@@ -33,8 +36,7 @@ sudo apt-get install -y chromium-chromedriver
 sudo apt-get install -y musl-tools numactl gdb
 
 ## for transmission: libcurl-dev?
-sudo apt install -y libssl-dev libevent-dev
-sudo apt install -y libclang-dev
+# sudo apt install -y libssl-dev libevent-dev
 
 sudo apt install -y jq
 
