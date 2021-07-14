2741f5bc942b5b17a43fe3974482879e97879d96
Shuwen Sun <sun.shuw@northeastern.edu>
2021-07-13 20:28:46 -0400

    auditbox script

---
+++ setup-scripts/auditbox-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p $HOME/dev
+
+# ioctl 0.3.3
+cd $HOME/dev
+git clone https://github.com/emberian/ioctl.git
+cd ioctl
+git checkout 70b01fe3e530ee4a582d7a1c79c4fc22eb903d97
+
+# rust sgx
+cd $HOME/dev
+git clone https://github.com/fortanix/rust-sgx
+cd rust-sgx
+git checkout 1575f9e3b4f2d241267c28fda7a0151b59be48cf
+
+# rust version
+rustup override set nightly-2017-01-26
