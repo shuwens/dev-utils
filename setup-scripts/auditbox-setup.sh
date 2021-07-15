ad48ae40a823b66a031f4046aabf4783d6fb5f13
Shuwen Sun <sun.shuw@northeastern.edu>
2021-07-15 14:21:18 -0400

    Popping stash

--- setup-scripts/auditbox-setup.sh
+++ setup-scripts/auditbox-setup.sh
@@ -12,9 +12,8 @@ git checkout 70b01fe3e530ee4a582d7a1c79c4fc22eb903d97
 
 # rust sgx
 cd $HOME/dev
-git clone https://github.com/fortanix/rust-sgx
+git clone https://github.com/jethrosun/rust-sgx -b safebricks-ver
 cd rust-sgx
-git checkout 1575f9e3b4f2d241267c28fda7a0151b59be48cf
 
 # rust version
 rustup toolchain install nightly-2017-01-26
