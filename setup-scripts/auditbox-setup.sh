ba2aadea4949d798e8216489415c143d00e9a734
Shuwen Sun <sun.shuw@northeastern.edu>
2021-07-15 15:20:45 -0400

    We Had To Use Dark Magic To Make This Work

--- setup-scripts/auditbox-setup.sh
+++ setup-scripts/auditbox-setup.sh
@@ -3,6 +3,18 @@
 set -e
 
 mkdir -p $HOME/dev
+cd $HOME/dev
+
+if [ -e "ioctl" ]; then
+  rm -rf ioctl
+  rm -rf pkg-config
+  rm -rf rust-openssl
+  rm -rf rust-sgx
+else
+  echo "Passing, no Cargo.lock.."
+fi
+
+
 
 # ioctl 0.3.3
 cd $HOME/dev
@@ -10,11 +22,18 @@ git clone https://github.com/emberian/ioctl.git
 cd ioctl
 git checkout 70b01fe3e530ee4a582d7a1c79c4fc22eb903d97
 
+
 # pkg-config 0.3.8
 cd $HOME/dev
 git clone https://github.com/rust-lang/pkg-config-rs.git
 cd pkg-config-rs
 git checkout 0.3.8
+
+
+# get openssl-sys so we can still use pkg-config 0.3.8
+cd $HOME/dev
+git clone https://github.com/jethrosun/rust-openssl.git -b safebricks-ver
+
 
 # rust sgx
 cd $HOME/dev
