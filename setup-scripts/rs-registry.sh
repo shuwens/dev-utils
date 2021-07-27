159757b2c8b78c12571bde20bdb6744691383839
Shuwen Sun <sun.shuw@northeastern.edu>
2021-07-27 16:34:01 -0400

    It's possible! you can turn a 50-line code chunk into just 3 lines. Here's how

--- setup-scripts/rs-registry.sh
+++ setup-scripts/rs-registry.sh
@@ -19,14 +19,23 @@ mkdir -p .cargo
 cat <<EOF > .cargo/config
   [source.crates-io]
   registry = "file://$HOME/dev/others/crates-io-rewound"
+
+  [build]
+  target-dir = "$HOME/data/cargo-target"
+
+  [term]
+  color = 'auto'
 EOF
 cat .cargo/config
+
+mkdir -p $HOME/dev/others
+mkdir -p $HOME/data
+
 # create copy of crates.io, reset back to when rand 0.8.0 didn't
 # exist in index.
 if [ -e "$HOME/dev/others/crates-io-rewound" ]; then
   echo "Great, we already have a crates io rewound .."
 else
-  mkdir -p $HOME/dev/others
   cd $HOME/dev/others
   git clone https://github.com/rust-lang/crates.io-index.git crates-io-rewound
 fi
