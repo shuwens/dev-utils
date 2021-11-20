7f37e982fed20ac213a67b6f002bcdce9ab97f00
Shuwen Sun <sun.shuw@northeastern.edu>
2021-11-20 14:30:49 -0500

    Feed. You. Stuff. No time.

--- setup-scripts/rust-setup.sh
+++ setup-scripts/rust-setup.sh
@@ -11,11 +11,11 @@ sudo apt-get install libssl-dev pkg-config cmake zlib1g-dev -y
 if [ -e $HOME/.cargo/ ]; then
 	echo "Passing, Rust already exists.."
 	source $HOME/.cargo/env
-	rustup default nightly
+	rustup default nightly-2020-12-25-x86_64-unknown-linux-gnu
 else
 	# setup rust
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
-	rustup toolchain install nightly --allow-downgrade --profile minimal --component clippy rust-src rustfmt miri
+	rustup toolchain install nightly-2020-12-25-x86_64-unknown-linux-gnu --allow-downgrade --profile minimal --component rust-src rustfmt
 	source $HOME/.cargo/env
 fi
 
